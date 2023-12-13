daf07f47be1c857dd455b787331537fc12aa7718
Omer Siman Tov <73315088+omersimantov@users.noreply.github.com>
2023-12-13 18:18:58 +0200

    Add field validation for the painting model

--- server/app/models/painting.rb
+++ server/app/models/painting.rb
@@ -1,2 +1,8 @@
 class Painting < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 2, maximum: 255 }
+  validates :description, length: { maximum: 2000 }, allow_blank: true
+  validates :image_url, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp }
+  validates :painter, presence: true, length: { minimum: 2, maximum: 255 }
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :order_link, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp }
 end
