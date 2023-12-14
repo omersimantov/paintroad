65fa0301170c3e0cc3744c78fb5da92beadfeb7f
Omer Siman Tov <73315088+omersimantov@users.noreply.github.com>
2023-12-14 13:44:40 +0200

    Add a simple 404 indicator component

---
+++ client/src/components/NotFound.tsx
@@ -0,0 +1,7 @@
+export const NotFound: React.FC = () => {
+  return (
+    <div className="flex h-[calc(100vh-7.5rem)] w-full items-center justify-center font-serif text-xl font-medium text-black">
+      404
+    </div>
+  );
+};
