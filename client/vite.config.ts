47108721ec1299dead1607b8214ee76fb8ca474b
Omer Siman Tov <73315088+omersimantov@users.noreply.github.com>
2025-01-01 15:50:36 +0200

    Prepare Vite for production

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,12 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    global: {},
-  },
-  server: {
-    port: 5173,
-  },
+  base: "/",
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
