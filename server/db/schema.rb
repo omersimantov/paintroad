399f4fad5655b0bd1dcd6e4aa0250c747ec8f4cc
Omer Siman Tov <73315088+omersimantov@users.noreply.github.com>
2025-01-05 20:51:51 +0200

    Sqlite to Postgres

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2023_12_12_155940) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "paintings", force: :cascade do |t|
     t.string "name"
     t.text "description"
