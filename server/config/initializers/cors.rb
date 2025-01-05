b8b882515faedfea3c83ff1dbad38bf568134223
Omer Siman Tov <73315088+omersimantov@users.noreply.github.com>
2025-01-05 18:55:47 +0200

    Update cors origin

--- server/config/initializers/cors.rb
+++ server/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://paintroad.vercel.app"
+    origins "paintrd.up.railway.app"
 
     resource "*",
       headers: :any,
