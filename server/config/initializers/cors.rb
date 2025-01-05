0dac1eb52823f7a4226638c8a0d87c18d3bfa019
Omer Siman Tov <73315088+omersimantov@users.noreply.github.com>
2025-01-05 19:00:57 +0200

    Fix

--- server/config/initializers/cors.rb
+++ server/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://paintrd.up.railway.app"
+    origins "https://paintroad.up.railway.app"
 
     resource "*",
       headers: :any,
